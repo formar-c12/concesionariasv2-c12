66e6ef71ebc54ffe5b6550ff07c2841071251896
Jona <retratos.jonatan@gmail.com>
2021-11-19 16:55:21 -0300

    Sucursal

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
 
+/* Enrutadores */
 let indexRouter = require('./routes/index');
 let usersRouter = require('./routes/users');
+let sucursalesRouter = require('./routes/sucursales')
 
 let app = express();
 
@@ -19,8 +21,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+/* Middlewares de rutas */
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/sucursal', sucursalesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
